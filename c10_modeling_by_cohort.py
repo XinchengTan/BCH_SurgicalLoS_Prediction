c832f825c0731b0331a0b7b126546ca298c81bf2
Xincheng Tan <caratan1998@yahoo.com>
2022-03-01 23:08:12 -0500

    enable shared ktrial dataset for cohort modeling

--- c10_modeling_by_cohort.py
+++ c10_modeling_by_cohort.py
@@ -9,7 +9,42 @@ from c4_model_perf import MyScorer, append_perf_row_generic
 from c2_models import get_model_by_cohort
 
 
-def gen_cohort_datasets(dashb_df: pd.DataFrame, cohort_col: str, min_cohort_size=10, **kwargs) -> Dict[Hashable, Dataset]:
+# helper function to filter dashb_df by cohort count
+def filter_df_by_cohort_count(dashb_df: pd.DataFrame,  cohort_col: str, min_cohort_size: int):
+  cohort_groupby = dashb_df.groupby(cohort_col)
+  if min_cohort_size > 1:
+    filtered_cohort = cohort_groupby.filter(lambda x: len(x) >= min_cohort_size)
+    return filtered_cohort
+  return dashb_df
+
+
+# Generate 'ktrials' of cohort-to-test-indices mapping, save the shuffled dashb_df as well
+def gen_ktrial_cohort_test_idxs(dashb_df: pd.DataFrame, cohort_col: str, min_cohort_size=50, ktrials=10, test_pct=0.2):
+  assert cohort_col in {SURG_GROUP, PRIMARY_PROC}, f'cohort_col must be one of [{SURG_GROUP, PRIMARY_PROC}]!'
+  assert cohort_col in dashb_df.columns, f'{cohort_col} must exist in dashb_df columns!'
+
+  if min_cohort_size is not None:
+    assert min_cohort_size >= 0, 'min_cohort_size must be a non-negative int!'
+    dashb_df = filter_df_by_cohort_count(dashb_df, cohort_col, min_cohort_size)
+
+  kt_cohort2testIdxs = []  # [ dict{cohort: [idx0, idx1, ...]}, ...]
+  kt_pprocDf = []
+  for k in range(ktrials):
+    # save shuffled df
+    pproc_df = dashb_df.sample(frac=1).reset_index(drop=True)
+    kt_pprocDf.append(pproc_df)
+    # save test indices
+    cohort2testIdxs = {}
+    for pproc, pp_df in pproc_df.groupby(cohort_col):
+      n = pp_df.shape[0]
+      cohort2testIdxs[pproc] = np.random.choice(np.arange(n), int(n * test_pct), replace=False)
+    kt_cohort2testIdxs.append(cohort2testIdxs)
+
+  return kt_pprocDf, kt_cohort2testIdxs
+
+
+def gen_cohort_datasets(dashb_df: pd.DataFrame, cohort_col: str, min_cohort_size=10, cohort2testIdxs=None,
+                        **kwargs) -> Dict[Hashable, Dataset]:
   """
   Generates a mapping from cohort name to a Dataset object, with train & test set splitted.
   Decile-related features are computed only on the cohort cases.
@@ -17,6 +52,7 @@ def gen_cohort_datasets(dashb_df: pd.DataFrame, cohort_col: str, min_cohort_size
   :param dashb_df: a pd.Dataframe object containing all sources of information
   :param cohort_col: Either SURG_GROUP or PRIMARY_PROC
   :param min_cohort_size: minimum number of samples per cohort
+  :param cohort2testIdxs: a dict mapping cohort name to a numpy array of test indices to use for that cohort
   :param kwargs: input args for Dataset() instantiation
 
   :return: A mapping from cohort name to a Dataset object
@@ -25,19 +61,30 @@ def gen_cohort_datasets(dashb_df: pd.DataFrame, cohort_col: str, min_cohort_size
   assert cohort_col in dashb_df.columns, f'{cohort_col} must exist in dashb_df columns!'
   assert min_cohort_size >= 0, 'min_cohort_size must be a non-negative int!'
 
-  # Generate cohort to data_df mapping
-  cohort_groupby = dashb_df.groupby(cohort_col)
-  if min_cohort_size > 1:
-    cohort_groupby = cohort_groupby.filter(lambda x: len(x) >= min_cohort_size).groupby(cohort_col)
-  print('[c11 gen_cohort_dataset] Effective sample size: ', cohort_groupby.size().sum())
+  # Generate cohort to cohort_data_df mapping
+  cohort_groupby = filter_df_by_cohort_count(dashb_df, cohort_col, min_cohort_size).groupby(cohort_col)
+  print('[c10 gen_cohort_dataset] Effective sample size: ', cohort_groupby.size().sum())
 
   # Generate cohort to Dataset() mapping
   cohort_to_dataset = {}
-  for cohort, data_df in cohort_groupby:
+  cases_in_use = 0
+  for cohort, cohort_data_df in cohort_groupby:
     print('\n\n***Cohort: ', cohort, '\n\n')
-    dataset = Dataset(data_df, **kwargs)  # 'cohort_col' should not exist in onehot_cols in **kwargs
+    if cohort2testIdxs is not None:
+      dataset = Dataset(cohort_data_df, test_idxs=cohort2testIdxs[cohort], **kwargs)
+    else:
+      dataset = Dataset(cohort_data_df, **kwargs)  # 'cohort_col' should not exist in onehot_cols in **kwargs
+
+    # Leave out cohorts that are emptied due to 1. one-to-many cases cleaning 2. nan feature row dropping 3. unseen code
     if dataset.Xtrain.shape[0] > 0 and dataset.Xtest.shape[0] > 0:
       cohort_to_dataset[cohort] = dataset
+      cases_in_use += dataset.Xtrain.shape[0] + dataset.Xtest.shape[0]
+
+  print('=====================================================================================')
+  print(f'*Total input cohorts: {len(cohort_groupby)}')
+  print(f'*Actual cohort datasets: {len(cohort_to_dataset)}')
+  print(f'*{cases_in_use} cases are used out of {dashb_df.shape[0]} available cases')
+  print('=====================================================================================')
   return cohort_to_dataset
 
 
