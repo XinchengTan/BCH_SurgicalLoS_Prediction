9b60e7bad6ea822d4dbcafceaef1d5910708c074
Xincheng Tan <caratan1998@yahoo.com>
2022-03-23 18:03:08 -0400

    Finished deploy_test_main

--- deploy_test_main.py
+++ deploy_test_main.py
@@ -2,11 +2,12 @@
 import joblib
 import pickle
 import pandas as pd
+from sklearn.metrics import accuracy_score
 
 from c0_data_prepare import prepare_data
 from c1_data_preprocessing import Dataset
 from c1_feature_engineering import FeatureEngineeringModifier
-from c2_models import get_model
+from c2_models import SafeOneClassWrapper
 from globals import *
 from globals_fs import *
 
@@ -25,12 +26,19 @@ def load_o2m_df(fp) -> pd.DataFrame:
 
 
 # Load pretrained model
-def load_pretrained_model(fp):
-
-  return
+def load_pretrained_model(fp) -> SafeOneClassWrapper:
+  with open(fp, 'rb') as md_file:
+    clf = joblib.load(md_file)
+  return clf
 
 
 if __name__ == '__main__':
+  # ---------------------------------------IMPORTANT NOTES: -------------------------------------------------
+  # 1. Use data_fp = DATA_DIR / "outsample3_pseudo_deploy.csv" to simulate real testing environment
+  # 2. In the simulated testing mode, os_dataset.ytest is assigned a placeholder value of 1000,
+  #    since actual LoS and admission/discharge time are not available
+  # ---------------------------------------------------------------------------------------------------------
+
   # 1. Generate test set dataframe with all sources of information combined
   os_data_df = prepare_data(data_fp=DATA_DIR / "outsample3.csv",
                             cpt_fp=DATA_DIR / "cpt_os3.csv",
@@ -57,8 +65,13 @@ if __name__ == '__main__':
         f'os_dataset.Xtest shape: {os_dataset.Xtest.shape}, os_dataset.ytest shape: {os_dataset.ytest.shape}')
 
   # 4. Load pretrained model
-  Clf = load_pretrained_model('')
+  Clf = load_pretrained_model(PRETRAINED_CLFS_DIR / 'xgbclf.joblib')
   print(f'[test_main] Loaded pretrained model!')
 
   # 5. Apply pre-trained models and output predictions
-  
+  os_predicted_nnt = Clf.predict(os_dataset.Xtest)
+  print('[test_main] Predicted outcome on os_dataset.Xtest:\n', os_predicted_nnt)
+
+  # 6. Check prediction accuracy (meaningless if in simulated testing mode)
+  print(f'[test_main] Out-of-sample Test Accuracy: '
+        f'{"{:.1%}".format(accuracy_score(os_dataset.ytest, os_predicted_nnt))}')
