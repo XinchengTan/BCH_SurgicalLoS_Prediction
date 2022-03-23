d120f5815c4d31bf48947d001aadd2a21dfc701e
Xincheng Tan <caratan1998@yahoo.com>
2022-03-23 15:57:20 -0400

    updated deploy_test_main

--- deploy_test_main.py
+++ deploy_test_main.py
@@ -1,18 +1,64 @@
 # Deployment script for applying pre-trained models
+import joblib
+import pickle
 import pandas as pd
 
 from c0_data_prepare import prepare_data
 from c1_data_preprocessing import Dataset
+from c1_feature_engineering import FeatureEngineeringModifier
 from c2_models import get_model
 from globals import *
 from globals_fs import *
 
 
+# Load Meta Data
+def load_FeatureEngModifier(fp) -> FeatureEngineeringModifier:
+  with open(fp, 'rb') as file:
+    FtrEngMod = pickle.load(file)
+  return FtrEngMod
+
+
+# Load skip cases (one-to-many)
+def load_o2m_df(fp) -> pd.DataFrame:
+  o2m_df = pd.read_csv(fp)
+  return o2m_df
+
+
+# Load pretrained model
+def load_pretrained_model(fp):
+
+  return
+
+
 if __name__ == '__main__':
-  # Load test set dataframe
+  # 1. Generate test set dataframe with all sources of information combined
+  os_data_df = prepare_data(data_fp=DATA_DIR / "outsample3.csv",
+                            cpt_fp=DATA_DIR / "cpt_os3.csv",
+                            cpt_grp_fp=CPT_TO_CPTGROUP_FILE,
+                            ccsr_fp=DATA_DIR / "ccsr_os3.csv",
+                            medication_fp=DATA_DIR / "medication_os3.csv",
+                            exclude2021=False,
+                            force_weight=False)
+  print(f'[test_main] Loaded os_data_df! Shape: {os_data_df.shape}')
 
-  # Preprocess & Engineer Features on test data, using meta data from historical set --> Dataset() object
+  # 2. Load Meta Data generated based on historical set
+  FtrEngMod = load_FeatureEngModifier(FTR_ENG_MOD_FILE)
+  O2M_df = load_o2m_df(O2M_FILE)
 
-  # Apply pre-trained models and output predictions
+  # 3. Preprocess & Engineer Features on test data, using meta data from historical set --> Dataset() object
+  os_dataset = Dataset(os_data_df,
+                       outcome=NNT,
+                       ftr_cols=FtrEngMod.feature_cols,
+                       test_pct=1,
+                       target_features=FtrEngMod.feature_names,
+                       remove_o2m=(False, O2M_df),
+                       ftr_eng=FtrEngMod)
+  print(f'[test_main] Finished data preprocessing and feature engineering! '
+        f'os_dataset.Xtest shape: {os_dataset.Xtest.shape}, os_dataset.ytest shape: {os_dataset.ytest.shape}')
 
-  pass
+  # 4. Load pretrained model
+  Clf = load_pretrained_model('')
+  print(f'[test_main] Loaded pretrained model!')
+
+  # 5. Apply pre-trained models and output predictions
+  
--- globals_fs.py
+++ globals_fs.py
@@ -5,8 +5,13 @@ PROJECT_DIR = Path('/Users/caratan/Desktop/ML4Health/Dashboard-Project')
 DATA_HOME = PROJECT_DIR / 'Data_new_all'
 DATA_TRIALS_HOME = PROJECT_DIR / 'Data_ktrials'
 DATA_DIR = DATA_HOME / 'ModelInput'
+
 DEPLOY_DATA_DIR = PROJECT_DIR / 'Deployment_test_data'
 DEPLOY_DEP_FILES_DIR = PROJECT_DIR / 'Deployment_dep_files'
+CPT_TO_CPTGROUP_FILE = DEPLOY_DEP_FILES_DIR / 'cpt2group.csv'
+FTR_ENG_MOD_FILE = DEPLOY_DEP_FILES_DIR / 'hist_FtrEngMod.pkl'
+O2M_FILE = DEPLOY_DEP_FILES_DIR / 'skip_cases.csv'
+
 RESULT_DIR = PROJECT_DIR / 'Results'
 COHORTWISE_RESULTS_DIR = RESULT_DIR / 'Cohortwise'
 AGGREGATIVE_RESULTS_DIR = RESULT_DIR / 'Aggregative'
