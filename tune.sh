669fa86a8791456fe4a13a93e0f70ea563e1e9e7
Xincheng Tan <caratan1998@yahoo.com>
2022-04-19 17:20:30 -0400

    add bash script for hyperparam tuning

---
+++ tune.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#python3 deploy_tune_main.py --oh_cols ccsr --n_iter 100 --models rmf --percase_cnt_vars ccsr cpt --scaler none
+#python3 deploy_tune_main.py --oh_cols ccsr cpt --n_iter 100 --models rmf --percase_cnt_vars ccsr cpt --scaler none
+#python3 deploy_tune_main.py --oh_cols ccsr cptgrp --n_iter 100 --models rmf --percase_cnt_vars ccsr cpt --scaler none
+#python3 deploy_tune_main.py --oh_cols ccsr pproc --n_iter 100 --models rmf --percase_cnt_vars ccsr cpt --scaler none
+
+
+python3 deploy_tune_main.py --oh_cols ccsr --n_iter 5 --models rmf --percase_cnt_vars ccsr cpt med123 --scaler none
+python3 deploy_tune_main.py --oh_cols ccsr cpt --n_iter 5 --models rmf --percase_cnt_vars ccsr cpt med123 --scaler none
+python3 deploy_tune_main.py --oh_cols ccsr cptgrp --n_iter 5 --models rmf --percase_cnt_vars ccsr cpt med123 --scaler none
+python3 deploy_tune_main.py --oh_cols ccsr pproc --n_iter 5 --models rmf --percase_cnt_vars ccsr cpt med123 --scaler none
+
