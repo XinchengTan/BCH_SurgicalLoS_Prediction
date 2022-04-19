b5bb47192bd6c0765e7f70c28b351c493454cc2b
Xincheng Tan <caratan1998@yahoo.com>
2022-04-19 15:59:26 -0400

    update pproc-los ~ outcome violin plot

--- eda/eda_proc.py
+++ eda/eda_proc.py
@@ -12,7 +12,7 @@ from globals import *
 from c1_data_preprocessing import gen_y_nnt
 
 
-
+# ------------------------------------------ CPT Count - Outcome ------------------------------------------
 def cpt_count_eda(dashb_df, outcome=NNT, preprocess_y=True):
   figs, axs = plt.subplots(2, 1, figsize=(12, 16))
   df = dashb_df[[outcome, CPTS]]
@@ -52,7 +52,7 @@ def cpt_count_eda(dashb_df, outcome=NNT, preprocess_y=True):
                          fontsize=14)
 
 
-
+# ----------------------------------------- Top-k Primary Procedure - Outcome -----------------------------------------
 def pproc_eda(dashb_df: pd.DataFrame, outcome=LOS, topK=20, xlim=30):
   total = dashb_df.shape[0]
   df = dashb_df[[LOS, NNT, PRIMARY_PROC, 'SURG_CASE_KEY']]
@@ -114,7 +114,8 @@ def pproc_eda(dashb_df: pd.DataFrame, outcome=LOS, topK=20, xlim=30):
   plt.show()
 
 
-def pproc_los_median_eda(dashb_df, outcome=NNT, preprocess_y=True, min_cnt=5):
+# --------------------------------------- Primary Procedure LOS Median - Outcome ---------------------------------------
+def pproc_los_median_eda(dashb_df, outcome=NNT, preprocess_y=True, min_cnt=5, xlim=15, save=False):
   total = dashb_df.shape[0]
   df = dashb_df[[LOS, NNT, PRIMARY_PROC, 'SURG_CASE_KEY']]
   figs, axs = plt.subplots(1, 1, figsize=(12, 9))  # gridspec_kw={'width_ratios': [2, 1]}
@@ -135,14 +136,24 @@ def pproc_los_median_eda(dashb_df, outcome=NNT, preprocess_y=True, min_cnt=5):
   print('Remaining cases: ', pproc_to_los_stat['case_cnt'].sum())
 
   df_filtered = df.join(pproc_to_los_stat.set_index(PRIMARY_PROC), on=PRIMARY_PROC, how='inner')
-  sns.boxplot(data=df_filtered, x='los_median', y=NNT, orient='h', ax=ax)  # scale='width',
-  ax.set_title('Primary Procedure LOS Median vs Outcome Class', fontsize=18, y=1.01)
+  outlier_df = df_filtered[df_filtered['los_median'] >= xlim]
+  if xlim:
+    df_filtered[outcome] = df_filtered[outcome].apply(lambda x: x if x < xlim else xlim+random.uniform(0, 0.5))
+    ax.set_xlim([-0.8, xlim+0.8])
+  #sns.boxplot(data=df_filtered, x='los_median', y=NNT, orient='h', ax=ax)
+  sns.violinplot(data=df_filtered, x='los_median', y=NNT, orient='h', ax=ax, scale='width')
+  #ax.scatter(outliers_df['los_median'], outliers_df[NNT], facecolors='none', edgecolors='r')
+  ax.set_title('Primary Procedure LOS Median vs LOS Outcome', fontsize=18, y=1.01)
   ax.set_xlabel('LOS median per primary procedure', fontsize=16)
   ax.set_ylabel('LOS (outcome class)', fontsize=16)
   ax.set_yticks(sorted(df[outcome].unique()))
   ax.set_yticklabels(NNT_CLASS_LABELS, fontsize=14)
   ax.xaxis.set_tick_params(labelsize=14)
   ax.invert_yaxis()
+  plt.setp(ax.collections, alpha=.9)
+  if save:
+    plt.savefig('pproc_los_med_outcome.png', dpi=400)
+  return outlier_df
   #return df_filtered.head(10)
   #df['case_cnt'] = groupby['SURG_CASE_KEY'].transform('count')
 
