d3846b252149b3250fc334c9f7043501ff3dec08
Xincheng Tan <caratan1998@yahoo.com>
2022-04-19 17:21:50 -0400

    make tune.sh executable

--- tune.sh
+++ tune.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 #python3 deploy_tune_main.py --oh_cols ccsr --n_iter 100 --models rmf --percase_cnt_vars ccsr cpt --scaler none
 #python3 deploy_tune_main.py --oh_cols ccsr cpt --n_iter 100 --models rmf --percase_cnt_vars ccsr cpt --scaler none
