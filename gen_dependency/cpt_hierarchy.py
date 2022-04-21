d76a8506943a9d44db4481c1618bb18a671756de
Xincheng Tan <caratan1998@yahoo.com>
2022-04-20 20:52:33 -0400

    rewrite gen_cpt_mapping, prev version is buggy

--- gen_dependency/cpt_hierarchy.py
+++ gen_dependency/cpt_hierarchy.py
@@ -2,35 +2,63 @@ from collections import defaultdict
 
 import numpy as np
 import pandas as pd
+from globals_fs import *
 
 
-def gen_cpt_groups(cpt_df, granularity=7, verbose=True):
+def pad_cpt_map(cpt_df: pd.DataFrame, max_level=4, save_maxlevel_mapping=False):
+  assert np.all(cpt_df['bucket_1'].notnull()), 'Bucket-1 CPT Group cannot contain NA!'
+  assert max_level < 8, 'max_level is at most 7!'
+  cpt_df_padded = cpt_df.copy()
+  for level in range(2, max_level+1):
+    cur_level_col = f'bucket_{level}'
+    cpt_df_padded.loc[cpt_df_padded[cur_level_col].isna(), cur_level_col] = cpt_df_padded[f'bucket_{level-1}']
+    cpt_df_padded.loc[cpt_df_padded[cur_level_col] == 'Other Procedures', cur_level_col] = cpt_df_padded[f'bucket_{level-1}']
+
+  if save_maxlevel_mapping:
+    df = cpt_df_padded[['cpt_code', f'bucket_{max_level}']].rename(columns={'cpt_code': 'CPT_CODE',
+                                                                            f'bucket_{max_level}': 'CPT_GROUP'})
+    df.to_csv(DEPLOY_DEP_FILES_DIR / f'cpt2group_level{max_level}.csv', index=False)
+  return cpt_df_padded
+
+
+
+
+
+# --------------------------------- IGNORE THE FOLLOWING CODE! GOT WEIRD BUG SOMEHOW... --------------------------------
+def gen_cpt_groups(cpt_df, max_level=7, verbose=True):
   """
   Build a mapping between CPT category name and a list of CPT codes.
-  The most detailed group is from bucket_'granularity'. CPTs with Nan category
+  The most detailed group is from bucket_'max_level'. CPTs with Nan category
   at each level above are assigned to its lowest-level category.
 
   :param cpt_df:
-  :param granularity: an int between 1 and 7 (TODO: This is a key param to watch in EDA)
+  :param max_level: an int between 1 and 7 (TODO: This is a key param to watch in EDA)
 
   :return: a mapping from CPT category to a list of CPTs
   """
-  assert 1 <= granularity <= 7, "granularity is an int in [1, 7]"
-  cpt_init = cpt_df[cpt_df['bucket_%d'%granularity].notnull()].groupby(['bucket_%d'%granularity])
+  assert 1 <= max_level <= 7, "max_level is an int in [1, 7]"
+  cpt_init = cpt_df[cpt_df[f'bucket_{max_level}'].notnull()].groupby([f'bucket_{max_level}'])
   cpt_groups = {k: list(v) for k, v in cpt_init.groups.items()}  # value: a list of CPT code indices = d
   cpt_groups = defaultdict(list, cpt_groups)
-  if granularity == 1:
+  if max_level == 1:
     return cpt_groups
+  if verbose:
+    print(f"Level {max_level}: #cpts covered in grouping: ", sum(len(v) for v in cpt_groups.values()))
+    # print("Number of CPTs with level %d category: %d" % (i, CPT_TOTAL - cpt_map['bucket_%d' % i].isnull().sum()))
+    print("Number of unique CPT categories: %d\n" % cpt_df[f'bucket_{max_level}'].nunique(dropna=True), "\n")
 
+  # Iterate bottom up, starting from max_level initialized in cpt_groups
   dup_cpt_grp = defaultdict(list)  # a mapping of CPT group name to a list of bucket levels that it occurs
-  for i in range(granularity-1, 0, -1):
+  for i in range(max_level-1, 0, -1):
+    print('Level ', i)
     if i > 1:
-      cpt_i = cpt_df[(cpt_df['bucket_%d'%(i + 1)].isnull()) & (cpt_df['bucket_%d'%i].notnull())].groupby(['bucket_%d'%i])
+      cpt_i = cpt_df[(cpt_df[f'bucket_{i+1}'].isnull()) & (cpt_df[f'bucket_{i}'].notnull())].groupby([f'bucket_{i}'])
     else:
       # add cpts with only bucket 1 to the singleton groups (bucket_1 is never null)
       cpt_i = cpt_df[cpt_df['bucket_2'].isnull()].groupby(['cpt_description'])
 
     # update dictionaries
+    # groupby.groups.items: [(group_name, Int64Index([cpt_df_idx0, cpt_df_idx1, ... ]))]
     for k, v in cpt_i.groups.items():
       if k != 'Other Procedures':
         if k not in cpt_groups.keys():
@@ -41,13 +69,13 @@ def gen_cpt_groups(cpt_df, granularity=7, verbose=True):
           dup_cpt_grp[k].append(i)  # Might falsely find a duplicate whose key is from the previous bucket of 'Other Procedures'
       else:
         # assign all CPT indices to the (i-1)th bucket
-        for df_idx in v:
-          actual_k = cpt_df.iloc[df_idx][i]
-          cpt_groups[actual_k].append(df_idx)
+        for cpt_df_idx in v:
+          prev_level_k = cpt_df.iloc[cpt_df_idx][i]
+          cpt_groups[prev_level_k].append(cpt_df_idx)
     if verbose:
       print("Level %d: #cpts covered in grouping: " % i, sum(len(v) for v in cpt_groups.values()))
       #print("Number of CPTs with level %d category: %d" % (i, CPT_TOTAL - cpt_map['bucket_%d' % i].isnull().sum()))
-      print("Number of unique CPT categories: %d\n" % cpt_df['bucket_%d' % i].nunique(dropna=True), "\n")
+      print("Number of unique CPT categories: %d\n" % cpt_df[f'bucket_{i}'].nunique(dropna=True), "\n")
 
   if verbose:
     print("Duplicated CPT groups and their occurrence bucket levels:")
@@ -56,14 +84,15 @@ def gen_cpt_groups(cpt_df, granularity=7, verbose=True):
       complete_levels = list(levels)
       for l in levels:
         for i in range(l, 8):
-          if k in cpt_df['bucket_%d'%i].unique() and i not in levels:
+          if k in cpt_df[f'bucket_{i}'].unique() and i not in levels:
             complete_levels.append(i)
       # If len == 1, it's actually not a duplicate
       if len(complete_levels) > 1:
         print(k, complete_levels)
         correct_dup2levels[k] = complete_levels
+    return cpt_groups, correct_dup2levels
 
-  return cpt_groups, correct_dup2levels
+  return cpt_groups
 
 
 def gen_groupCode_mappings(cpt_df, cpt_groups):
@@ -93,7 +122,7 @@ def is_valid_cpt_grouping(cpt_df, group2cpts):
   for k, v in group2cpts.items():
     for c in v:
       if c not in all_cpt_codes:
-        raise ValueError("Found code not in CPT dataframe! %d" % c)
+        raise ValueError(f"Found code not in CPT dataframe! {c}")
       idx = all_cpt_codes.index(c)
       cpt_markers[idx] = 1 - cpt_markers[idx]  # flip the bit
 
